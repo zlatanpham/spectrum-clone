ddf1ed95bb6fac64d81058528a8aeb6c988f9dd5
zlatan <zlatan.pham@gmail.com>
2020-01-14 09:21:04 +0700

    ci(client): setup github action for storybook

--- client/components/ExampleComponent/ExampleComponent.stories.tsx
+++ client/components/ExampleComponent/ExampleComponent.stories.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import ExampleComponent from '.';
 
 storiesOf('components/ExampleComponent', module).add('Basic', () => (
-  <ExampleComponent>Test</ExampleComponent>
+  <ExampleComponent>Test deploy</ExampleComponent>
 ));
