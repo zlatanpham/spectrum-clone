8893e75ac86a68d9171f88aa7ff209fb04f4a289
zlatan <zlatan.pham@gmail.com>
2020-02-08 23:14:38 +0700

    feat(client): add storybook for icons

--- client/components/icons/createIcon.tsx
+++ client/components/icons/createIcon.tsx
@@ -2,26 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 
 const InlineSvg = styled.svg`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
   color: inherit;
   fill: currentColor;
 `;
 
+const SvgWrapper = styled.div<{ size: number }>`
+  display: inline-block;
+  flex: 0 0 ${props => (props.size ? `${props.size}px` : '32px')};
+  width: ${props => (props.size ? `${props.size}px` : '32px')};
+  height: ${props => (props.size ? `${props.size}px` : '32px')};
+  min-width: ${props => (props.size ? `${props.size}px` : '32px')};
+  min-height: ${props => (props.size ? `${props.size}px` : '32px')};
+  position: relative;
+  color: inherit;
+`;
+
 export default function createIcon(svg: React.ReactNode) {
-  return (props: React.HTMLAttributes<SVGElement>) => (
-    <InlineSvg
-      fillRule="evenodd"
-      clipRule="evenodd"
-      strokeLinejoin="round"
-      strokeMiterlimit="1.414"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-labelledby="title"
-      viewBox="0 0 32 32"
-      width="1em"
-      height="1em"
-      preserveAspectRatio="xMidYMid meet"
-      {...props}
-    >
-      {svg}
-    </InlineSvg>
+  return ({
+    size = 32,
+    ...props
+  }: { size?: number } & React.HTMLAttributes<SVGElement>) => (
+    <SvgWrapper size={size}>
+      <InlineSvg
+        fillRule="evenodd"
+        clipRule="evenodd"
+        strokeLinejoin="round"
+        strokeMiterlimit="1.414"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-labelledby="title"
+        viewBox="0 0 32 32"
+        preserveAspectRatio="xMidYMid meet"
+        {...props}
+      >
+        {svg}
+      </InlineSvg>
+    </SvgWrapper>
   );
 }
