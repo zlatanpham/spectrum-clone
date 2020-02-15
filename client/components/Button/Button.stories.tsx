cddfde21ff162a6b32fb3cd9596462cd39a477a5
zlatan <zlatan.pham@gmail.com>
2020-02-15 13:58:25 +0700

    feat(client): scaffold new post layout

--- client/components/Button/Button.stories.tsx
+++ client/components/Button/Button.stories.tsx
@@ -5,12 +5,14 @@ import OutlineButton from './OutlineButton';
 import PrimaryOutlineButton from './PrimaryOutlineButton';
 import WarnButton from './WarnButton';
 import WhiteButton from './WhiteButton';
+import TransparentButton from './TransparentButton';
 
 storiesOf('components/Button', module).add('Button', () => (
   <>
     <div className="p-5 bg-white flex">
       <PrimaryButton>Hello World</PrimaryButton>
       <OutlineButton>Hello World</OutlineButton>
+      <TransparentButton>Hello World</TransparentButton>
       <PrimaryOutlineButton>Hello World</PrimaryOutlineButton>
       <WarnButton>Hello World</WarnButton>
       <WhiteButton>Hello World</WhiteButton>
@@ -18,6 +20,7 @@ storiesOf('components/Button', module).add('Button', () => (
     <div className="p-5 bg-white flex">
       <PrimaryButton size="small">Hello World</PrimaryButton>
       <OutlineButton size="small">Hello World</OutlineButton>
+      <TransparentButton size="small">Hello World</TransparentButton>
       <PrimaryOutlineButton size="small">Hello World</PrimaryOutlineButton>
       <WarnButton size="small">Hello World</WarnButton>
       <WhiteButton size="small">Hello World</WhiteButton>
--- client/pages/new/post/index.tsx
+++ client/pages/new/post/index.tsx
@@ -1,6 +1,82 @@
-import React from 'react';
-import SingleColumnLayout from 'components/Layout/SingleColumnLayout';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+import Select, { OptionItem } from 'components/Select';
+import IconMarkdown from 'components/icons/IconMarkdown';
+import PrimaryButton from 'components/Button/PrimaryButton';
+import IconPhoto from 'components/icons/IconPhoto';
+import TransparentButton from 'components/Button/TransparentButton';
+
+const Container = styled.div`
+  ${tw`fixed inset-0 flex justify-center`}
+  background: rgba(0, 0, 0, 0.24);
+`;
+
+const Body = styled.div`
+  ${tw`bg-white h-screen`}
+  z-index: 9995;
+  box-shadow: rgba(0, 0, 0, 0.08) -4px 0px 12px,
+    rgba(0, 0, 0, 0.08) 4px 0px 12px;
+  background: rgb(250, 250, 250);
+  width: 100%;
+  max-width: 1424px;
+`;
+
+const Header = styled.header`
+  ${tw`flex items-center py-2 pr-2 pl-3 bg-gray-200 border border-b`}
+  height:48px;
+`;
+
+const FormContent = styled.div`
+  height: calc(100vh - 48px - 56px);
+`;
+
+const Footer = styled.footer`
+  ${tw`flex items-center justify-between py-2 px-3 bg-gray-200 border border-t`}
+  height:56px;
+`;
+
+const communityOptions: OptionItem[] = [
+  { name: 'Choose a community', value: '' },
+  { name: 'React', value: 'react' },
+  { name: 'Sketch', value: 'sketch' },
+];
 
 export default function NewPost() {
-  return <SingleColumnLayout>Create New Post</SingleColumnLayout>;
+  const [community, setCommunity] = useState('');
+  return (
+    <Container>
+      <Body>
+        <Header>
+          <div>
+            <span className="text-sm font-medium">Post to:</span>
+            <Select
+              options={communityOptions}
+              scheme="purple"
+              value={community}
+              onChange={setCommunity}
+            />
+          </div>
+        </Header>
+        <FormContent>Tab</FormContent>
+        <Footer>
+          <a
+            className="inline-flex text-gray items-center"
+            href="https://guides.github.com/features/mastering-markdown/"
+            target="_blank"
+            rel="noopener"
+          >
+            <IconPhoto />
+            <IconMarkdown className="ml-1" />
+          </a>
+          <div className="flex items-center">
+            <TransparentButton className="mr-1" to="/explore">
+              Cancel
+            </TransparentButton>
+            <PrimaryButton>Publish</PrimaryButton>
+          </div>
+        </Footer>
+      </Body>
+    </Container>
+  );
 }
